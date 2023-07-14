e4fa7f29e41d68ab4fd78d77185f50cb180d9538
Lamees <lameesdavidson41@gmail.com>
2023-07-14 12:57:30 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,18 @@
  * Return: Always 0
  */
 int main(void)
-{
-	int n;
-
-	for (n = 49; n <= 57; n++)
 	{
-		putchar(n);
-		if (n == 57)
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
 		{
-			continue;
+			putchar(',');
+			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
-}
+	}
