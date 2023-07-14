57bc2e82a6de8f4eaa5aca792c140bd05d9ba2d2
Lamees <lameesdavidson41@gmail.com>
2023-07-14 13:21:59 -0700

    print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'print comb of numbers'
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 0; a <= 10; a++)
+	for (i = 0 ; i <= 10 ; i++)
 	{
-		putchar(a + '0');
-		if (a < 9)
-		{
-			break;
-		}
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
